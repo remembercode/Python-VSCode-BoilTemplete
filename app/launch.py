13556fb692c663b3e7f73d825ecfd317a74adce3
Wang Bing <remembercode@live.cn>
2024-06-18 01:12:09 +0800

    add cwd and import to main for dev

--- app/launch.py
+++ app/launch.py
@@ -1,4 +1,10 @@
 import sys
+import os
+
+# add app to lib path for import
+# 增加app目录到搜索路径中，方便后续的项目import自己写的py文件lib
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(parent_dir)
 
 def main():
     print(sys.version_info)
