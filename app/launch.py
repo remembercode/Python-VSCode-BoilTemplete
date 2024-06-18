6f5ce1d14dd2e93cda00d3486b304dff0d276282
Wang Bing <remembercode@live.cn>
2024-06-18 13:50:23 +0800

    app.py作为主文件，lunch.py增加当前路径作为lib搜索路径，便于后续import导入app文件夹下的lib目录

--- app/launch.py
+++ app/launch.py
@@ -1,14 +1,9 @@
 import sys
 import os
+import app
 
-# add app to lib path for import
-# 增加app目录到搜索路径中，方便后续的项目import自己写的py文件lib
+# 将tts模块的父目录添加到sys.path中
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(parent_dir)
 
-def main():
-    print(sys.version_info)
-    print("main start !")
-
-if __name__ == "__main__":
-    main()
+app.main()
